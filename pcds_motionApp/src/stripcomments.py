2dfc261c9775cd0ccf0b33d837bd0674ae28f8e9
alexd <alexd@slac.stanford.edu>
2013-09-26 20:26:55 +0000

    Changed Ud, Ld to counters rather than flags, they count the number of violations,counters are cleared when se=0 is called
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/modules/pcds_motion/current@14039 ececa60f-9f63-0410-97c7-ea05888eb260

--- pcds_motionApp/src/stripcomments.py
+++ pcds_motionApp/src/stripcomments.py
@@ -1,8 +1,13 @@
+import time
+
 inFile = open('ims_bootup_V3.mcode.klg', 'r')
 outFile = open('ims_bootup_V3.mcode', 'w')
 
 line = inFile.readline()
-
+print "Take this comments"
+time.sleep(.5)
+print "POW!"
+time.sleep(.5)
 while line:
     if line[0] != '\'':
         if '\'' in line:
@@ -11,6 +16,9 @@ while line:
 	if line.__len__() != 0:
             outFile.write(line + '\n')
     line = inFile.readline()
-
+print "And this"
+time.sleep(.5)
+print "KA-POW!!"
+time.sleep(.5)
 inFile.close()
 outFile.close()
