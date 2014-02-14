6ee84f4ac6285963444bc173f149ba89d3d06cf0
Bruce Hill <bhill@slac.stanford.edu>
2014-02-14 06:40:26 +0000

    Fixed build for RTEMS and misc build cleanups
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/modules/pcds_motion/current@15828 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm-test-interlocks.cmd
+++ edm-test-interlocks.cmd
@@ -1,20 +1,7 @@
 #! /bin/bash
 
 # Setup edm environment
-export EPICS_SITE_TOP=/reg/g/pcds/package/epics/3.14
-export EPICS_HOST_ARCH=$($EPICS_SITE_TOP/base/current/startup/EpicsHostArch.pl)
-
-export EDMFILES=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMFILTERS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMHELPFILES=$EPICS_SITE_TOP/extensions/current/src/edm/helpFiles
-export EDMLIBS=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
-export EDMOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMPVOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMUSERLIB=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
-export EDMACTIONS=/reg/g/pcds/package/tools/edm/config
-export EDMWEBBROWSER=mozilla
-export PATH=$PATH:$EPICS_SITE_TOP/extensions/current/bin/$EPICS_HOST_ARCH
-export EDMDATAFILES=".:.."
+source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
 edm -x -eolc		\
  -m "LLL=SXR"		\
