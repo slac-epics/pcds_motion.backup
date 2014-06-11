842f223ae6713fc0263bffb0b1c2eed480e5c5ed
Karl Gumerlock <klg@slac.stanford.edu>
2014-06-11 23:58:31 +0000

    New versions of IMS firmware for Pulse Picker, as well as new version of comment stripping utility.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/modules/pcds_motion/current@17455 ececa60f-9f63-0410-97c7-ea05888eb260

--- pcds_motionApp/src/mcode_archive/stripcomments.py
+++ pcds_motionApp/src/mcode_archive/stripcomments.py
@@ -1,24 +1,68 @@
+# MCode Firmware Comment Stripper
+# ------------------------------------------------------------------------
+# Author: Karl Gumerlock
+#
+# This utility is necessary to economize space when uploading firmware
+# to IMS/Schneider Electric MCode-powered products. If comments are
+# uploaded to the controller, they reside in flash. What's the point of
+# that?
+#
 import time
+import sys
 
-inFile = open('ims_bootup_V3.mcode.klg', 'r')
-outFile = open('ims_bootup_V3.mcode', 'w')
+if len(sys.argv) == 1:
+    # No arguments passed
+    print "MCode Firmware Comment Stripper"
+    print "Usage: stripcomments.py INPUT_FILE [OUTPUT_FILE]"
+    sys.exit(2)
+
+if len(sys.argv) > 3:
+    # Too many arguments
+    print "ERROR: Too many arguments; check your syntax."
+    sys.exit(3)
+
+# Open input file
+try:
+    inFile = open(sys.argv[1], 'r')
+except IOError:
+    print "ERROR: Could not open input file " + sys.argv[1]
+    sys.exit(4)
+
+# If necessary, open output file
+if len(sys.argv) == 3:
+    try:
+        outFile = open(sys.argv[2], 'w')
+    except IOError:
+        print "ERROR: Could not open output file " + sys.argv[2]
+        sys.exit(5)
+
 
 line = inFile.readline()
-print "Take this comments"
-time.sleep(.5)
-print "POW!"
-time.sleep(.5)
 while line:
+    # First catch all comment lines that start with '''
     if line[0] != '\'':
+        # Substring any other lines that have in-line comments
         if '\'' in line:
     	    line = line[:(line.find('\'')-1)]
+        # Strip away whitespace
         line = line.strip() 
-	if line.__len__() != 0:
-            outFile.write(line + '\n')
+        # Check if you're left with anything of consequence
+        if line.__len__() != 0:
+            if len(sys.argv) == 3:
+                try:
+                    # Output to file if argument was passed
+                    outFile.write(line + '\n')
+                except IOError:
+                    print "ERROR: Could not write to output file " \
+                          + sys.argv[2]
+                    sys.exit(6)
+            else:
+                # Otherwise output to stdout
+                print line
+
     line = inFile.readline()
-print "And this"
-time.sleep(.5)
-print "KA-POW!!"
-time.sleep(.5)
+
 inFile.close()
-outFile.close()
+
+if len(sys.argv) == 3:
+    outFile.close()
